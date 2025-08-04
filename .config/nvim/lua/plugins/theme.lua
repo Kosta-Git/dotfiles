cfbec28b67ad83f0b2cfe98bbb040db57d754252
kosta <k.sovaridis@inotek.be>
2025-08-04 14:39:59 +0200

    sync

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+        term_colors = true,
+        float = {
+          transparent = true,
+          solid = false,
+        },
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
